635717827ce286e31dcdcdc2d05327ada612c8bd
Fields <will.openfields@gmail.com>
2019-01-02 16:43:29 -0600

    need to do melt/cast to get data for observations properly formatted; need to add in the observer data for the points where species are unobserved

--- R/obs_script.r
+++ R/obs_script.r
@@ -1,5 +1,5 @@
 read.csv('./data/wilcox2017.csv', header=TRUE) -> d17
-
+library(dplyr)
 source('./R/spselect.r')
 spselect(code = "EATO", df = d17)
 
@@ -10,3 +10,20 @@ apply(SP.EATO[,12:13],1,sum) -> in60
 #dim(as.data.frame(in60))
 which(in60>0) -> nearobs
 SP.EATO[nearobs,2:11]
+
+# get sites that were surveyed where the species was not observed
+setdiff(unique(na.omit(d17$Point)), unique(SP.EATO$Point)) -> not.obs
+matrix(0, nrow=length(not.obs), ncol=3) -> nodata 
+nodata <- as.data.frame(nodata)
+names(nodata) <- c("Point", "Ob1", "Ob2")
+nodata[,1] <- not.obs # copy in point identifier
+
+
+
+# need to get list of sites and observers
+table(d17$Point, d17$Observer) -> p.ob
+table(d17$Point)
+
+# need to melt/cast data
+
+
